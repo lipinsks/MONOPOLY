8a00fa8cbcba97e8ae1edc8f94526a14ad031796
Artur Kolecki <koeckiartur@gmail.com>
2026-09-24 19:05:32 +0200

    enable permission enforcement in app configuration

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ FRONTEND_DIST = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fronte
 log = logging.getLogger('werkzeug')
 app = Flask(__name__, static_folder=FRONTEND_DIST, static_url_path='/static')
 
-ENFORCE_PERMISSIONS = False
+ENFORCE_PERMISSIONS = True
 rooms = {}
 
 try:
